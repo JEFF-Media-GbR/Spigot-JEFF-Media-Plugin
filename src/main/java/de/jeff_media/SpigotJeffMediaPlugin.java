f3e5068a017ba76239697d3a21ca1315b07dcc49
mfnalex <mail@jeff-media.de>
2021-03-27 12:03:20 +0100

    1.1.0 added getInstance()

--- src/main/java/de/jeff_media/SpigotJeffMediaPlugin.java
+++ src/main/java/de/jeff_media/SpigotJeffMediaPlugin.java
@@ -1,11 +1,16 @@
 package de.jeff_media;
 
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
 public interface SpigotJeffMediaPlugin {
 
     String getUID();
     String getNONCE();
     String getRESOURCE();
 
-    SpigotJeffMediaPlugin getInstance();
+    static Plugin getInstance() {
+        return null;
+    }
 
 }
